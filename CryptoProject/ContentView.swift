d7c1c875aed9d6f922fd83091bad29e84884fc92
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2025-12-30 11:03:04 +0300

    Initial Commit

---
+++ CryptoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoProject
+//
+//  Created by Илья Ануфриев on 30.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoProject/CryptoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoProjectApp.swift
+//  CryptoProject
+//
+//  Created by Илья Ануфриев on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
