6d53d0f7d6fe19b09251f0886df0df218c2b10f6
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2026-01-03 11:18:30 +0300

    Добавлен NetworkManager

--- CryptoProject/Services/CoinDataService.swift
+++ CryptoProject/Services/CoinDataService.swift
@@ -13,29 +13,12 @@ class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h)" ) else { return }
         
-       coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnCoins) in
                 guard let self = self else { return }
-                self.allCoins = returnedCoins
+                self.allCoins = returnCoins
                 self.coinSubscription?.cancel()
-            }
+            })
     }
 }
