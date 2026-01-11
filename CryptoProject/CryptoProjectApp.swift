ca7256ba7ae41fa16560908ddb18deb91e613890
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2026-01-11 13:45:36 +0300

    Реализован LaunchScreen и LoadingView

--- CryptoProject/CryptoProjectApp.swift
+++ CryptoProject/CryptoProjectApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct CryptoProjectApp: App {
     
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
@@ -12,10 +13,20 @@ struct CryptoProjectApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                HomeView()
+            ZStack {
+                NavigationStack {
+                    HomeView()
+                }
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
         }
     }
 }
